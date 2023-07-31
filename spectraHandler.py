5832588b4a552389c01726a23e16080e59903a56
frkatona <apk5101@psu.edu>
2023-07-31 11:49:55 -0400

    fixed group regions and color maps and added log fit

--- spectraHandler.py
+++ spectraHandler.py
@@ -1,12 +1,8 @@
-from distutils.command.build_scripts import first_line_re
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import glob, os
-
-import numpy as np
 from scipy.integrate import simps
-import matplotlib.colors as mcolors
 
 def WN_to_index(WN_array, WN):
     difference_array = np.absolute(WN_array - WN)
@@ -39,24 +35,23 @@ def PeakIntegration(WN_standardized, WN_array, WN_low, WN_high):
         areaarray.append(area - baseline_area)
     return areaarray
 
-
 readpath = r'CSVs'
 os.chdir(readpath)
 filelist = sorted(glob.glob('*.csv'))
 
-columnname = filelist[0][0:-4]
+columnname = filelist[0].split("_", 1)[1][:-4]
 df_tot = pd.read_csv(filelist[0], skiprows = 2, header = None, names = ['cm-1', columnname])
 df_tot = df_tot.sort_values(by=['cm-1'], ignore_index = True)
 
-WN_normal_low, WN_normal_high = 1260, 1263# 2957, 2967 CH3, goes great with Si-H
+WN_normal_low, WN_normal_high = 1260, 1263
 WN_baseline_low, WN_baseline_high = 3400, 3600
 
 WN_group = 0
-WN_low = [780, 800, 1000, 2080, 2900, 3060]
-WN_high = [810, 950, 1130, 2280, 2970, 3080]
+WN_low = [780, 830, 970, 2100, 2930, 3060]
+WN_high = [830, 930, 1150, 2225, 3000, 3080]
 groupname = ['Si-CH3', 'Si-H (bend)', 'Si-O-Si', 'Si-H (stretch)', 'CH3', 'vinyl (C=C)']
 
-df_area = pd.DataFrame(index = groupname)
+df_area = pd.DataFrame(index=groupname)
 
 WN_array = df_tot['cm-1'].to_numpy()
 index_baseline_low = WN_to_index(WN_array, WN_baseline_low)
@@ -64,38 +59,79 @@ index_baseline_high = WN_to_index(WN_array, WN_baseline_high)
 index_normal_low = WN_to_index(WN_array, WN_normal_low)
 index_normal_high = WN_to_index(WN_array, WN_normal_high)
 
-cmap1 = plt.get_cmap("jet")  # colormap for '0cb'
-cmap2 = plt.get_cmap("jet")  # colormap for '5e-3'
+num_samples = len(filelist)
+
+fig_raw, ax_raw = plt.subplots()
+fig_stand, ax_stand = plt.subplots()
+
+color_list = []
+cubehelix_palette = plt.cm.plasma(np.linspace(0, 1, len(WN_low)))
+
+# Define x arrays for each scatter plot
+x_si_h_stretch_0cb = []
+x_si_h_stretch_5e3 = []
+
+y_si_h_stretch_0cb = []
+y_si_h_stretch_5e3 = []
 
 for i, file in enumerate(filelist):
-    columnname = file[0:-4]
+    columnname = file.split("_", 1)[1][:-4]
 
-    df_add = pd.read_csv(file, skiprows = 2, header = None, names = ['cm-1', columnname])
-    df_add = df_add.sort_values(by=['cm-1'], ignore_index = True)
+    df_add = pd.read_csv(file, skiprows=2, header=None, names=['cm-1', columnname])
+    df_add = df_add.sort_values(by=['cm-1'], ignore_index=True)
     WN_raw = df_add[columnname].to_numpy()
     WN_standardized = Standardize(WN_raw, index_baseline_low, index_baseline_high, index_normal_low, index_normal_high)
     
     df_tot[columnname] = WN_standardized
-    df_area[columnname] = PeakIntegration(WN_standardized, WN_array, WN_low, WN_high)
+    area = PeakIntegration(WN_standardized, WN_array, WN_low, WN_high)
+    df_area[columnname] = area
 
-    # Calculate the color step size
-    color_step = i / len(filelist)
-
-    if file == filelist[0]:
-        if '0cb' in file:
-            ax_raw = df_add.plot('cm-1', columnname, title = 'Raw Spectra', color=cmap1(color_step))
-            ax_stand = df_tot.plot('cm-1', columnname, title = 'Corrected Spectra', color=cmap1(color_step))
-        elif '5e-3' in file:
-            ax_raw = df_add.plot('cm-1', columnname, title = 'Raw Spectra', color=cmap2(color_step))
-            ax_stand = df_tot.plot('cm-1', columnname, title = 'Corrected Spectra', color=cmap2(color_step))
+    if "5e-3" in file:
+        color = plt.cm.jet(i/num_samples)
+        y_si_h_stretch_5e3.append(area[3])  # Si-H (stretch) index is 3
+        x_si_h_stretch_5e3.append(i)  # x values for scatter plot
     else:
-        if '0cb' in file:
-            df_add.plot('cm-1', columnname, ax = ax_raw, color=cmap1(color_step))
-            df_tot.plot('cm-1', columnname, ax = ax_stand, color=cmap1(color_step))
-        elif '5e-3' in file:
-            df_add.plot('cm-1', columnname, ax = ax_raw, color=cmap2(color_step))
-            df_tot.plot('cm-1', columnname, ax = ax_stand, color=cmap2(color_step))
+        color = plt.cm.viridis(i/num_samples)
+        y_si_h_stretch_0cb.append(area[3])  # Si-H (stretch) index is 3
+        x_si_h_stretch_0cb.append(i)  # x values for scatter plot
 
-ax_area = df_area.plot.bar(title = 'Peak Areas', rot = 30)
+    color_list.append(color)
+    
+    df_add.plot('cm-1', columnname, ax=ax_raw, color=color)
+    df_tot.plot('cm-1', columnname, ax=ax_stand, color=color)
+
+for j in range(len(WN_low)):
+    ax_stand.axvline(x=WN_low[j], color=cubehelix_palette[j], linestyle='--', linewidth=2)
+    ax_stand.axvline(x=WN_high[j], color=cubehelix_palette[j], linestyle='--', linewidth=2)
+
+ax_raw.set_title('Raw Spectra')
+ax_stand.set_title('Corrected Spectra')
+
+ax_area = df_area.plot.bar(title='Peak Areas', rot=30, color=color_list)
+
+fig_scatter, ax_scatter = plt.subplots()
+ax_scatter.scatter(x_si_h_stretch_0cb, y_si_h_stretch_0cb, color=plt.cm.viridis(np.linspace(0, 1, len(y_si_h_stretch_0cb))))
+ax_scatter.scatter(x_si_h_stretch_5e3, y_si_h_stretch_5e3, color=plt.cm.jet(np.linspace(0, 1, len(y_si_h_stretch_5e3))))
+ax_scatter.set_title('Si-H Stretch Area')
+
+# Apply logarithmic fits and plot them
+
+# Exclude zero-valued data points to avoid -inf
+x_si_h_stretch_0cb_log_fit = [x for x, y in zip(x_si_h_stretch_0cb, y_si_h_stretch_0cb) if y != 0]
+y_si_h_stretch_0cb_log_fit = [y for y in y_si_h_stretch_0cb if y != 0]
+
+x_si_h_stretch_5e3_log_fit = [x for x, y in zip(x_si_h_stretch_5e3, y_si_h_stretch_5e3) if y != 0]
+y_si_h_stretch_5e3_log_fit = [y for y in y_si_h_stretch_5e3 if y != 0]
+
+# Fit a log curve to the data
+coefficients_0cb = np.polyfit(x_si_h_stretch_0cb_log_fit, np.log10(y_si_h_stretch_0cb_log_fit), 1)
+polynomial_0cb = np.poly1d(coefficients_0cb)
+y_fit_0cb = 10**polynomial_0cb(x_si_h_stretch_0cb_log_fit)
+ax_scatter.plot(x_si_h_stretch_0cb_log_fit, y_fit_0cb, color="green")
+
+coefficients_5e3 = np.polyfit(x_si_h_stretch_5e3_log_fit, np.log10(y_si_h_stretch_5e3_log_fit), 1)
+polynomial_5e3 = np.poly1d(coefficients_5e3)
+y_fit_5e3 = 10**polynomial_5e3(x_si_h_stretch_5e3_log_fit)
+ax_scatter.plot(x_si_h_stretch_5e3_log_fit, y_fit_5e3, color="red")
+
 plt.show()
-
